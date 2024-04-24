3ac52bce2926063211910ed3acdd8e8b8b200490
Vardhit <Vardhit>
2024-04-24 20:58:59 +0530

    React first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h2",{id:"heading"},"Hello Vardhit!!!"); // First create Element like h1 tag
+const root = ReactDOM.createRoot(document.getElementById("root")) // Then using React DOM create root in that getElementbyID that is root
+root.render(heading) //Finally we are rendering element that we have created for heading
